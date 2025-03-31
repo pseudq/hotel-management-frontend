62b63849ab8681d05cbc08b3e3dc48ac817bb418
Tran Bao Ngoc <tbngoc0100@gmail.com>
2025-03-31 09:20:13 +0700

    add docker, update api to env

---
+++ env.js
@@ -0,0 +1,4 @@
+// Add this file to your public directory
+window.env = {
+  REACT_APP_API_URL: "REACT_APP_API_URL_PLACEHOLDER",
+};
--- src/apiService.js
+++ src/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 // const API_URL = "http://3.0.95.227:5000/api";
 // const API_KEY = "your_api_key";
 
